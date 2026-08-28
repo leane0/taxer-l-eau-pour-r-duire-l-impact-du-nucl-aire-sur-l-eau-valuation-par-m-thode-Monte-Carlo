21d7ef483f1a3467332916a01e84729e0d8ec79f
Leane Pelletier <leane.pelletier@student-cs.fr>
2026-08-28 16:24:53 +0200

    fix

--- Suivi sites/montecarlo.py
+++ Suivi sites/montecarlo.py
@@ -6,7 +6,7 @@ from plot import *
 import os
 import pandas as pd
 import pickle
-from plot_param import plot_param
+from plot_param import *
 from sobol import *
 from tornado import *
 
@@ -29,66 +29,51 @@ def montecarlo_parameters(n_sim, cnpe_choice, scenario):
     opex_results = []
     redevance_results = []
     loss_prod_results = []
+    npv_results = []
+    roi_results =[]
 
     for i in range(n_sim):
 
         lifetime = np.random.choice(param["L"])
-
         capex = np.random.triangular(
             param["CAPEX_MIN"],
             param["CAPEX_MODE"],
             param["CAPEX_MAX"]
         )
-
         loss_2030 = np.random.triangular(
             param["PERTE_PROD_2030_MIN"],
             param["PERTE_PROD_2030_MODE"],
             param["PERTE_PROD_2030_MAX"]
         )
-
         loss_2050 = np.random.triangular(
             param["PERTE_PROD_2050_MIN"],
             param["PERTE_PROD_2050_MODE"],
             param["PERTE_PROD_2050_MAX"]
         )
-
         power = param["PUISSANCE"]
+        wacc=np.random.triangular(WACC_VALUES[0],WACC_VALUES[1],WACC_VALUES[2])
 
-        # -------------------------
         # OPEX
-        # -------------------------
         if cnpe_choice in ["Tricastin", "Saint-Alban", "Bugey"]:
-
             loss_opex = np.random.triangular(
                 param["PERTE_OPEX_MIN"],
                 param["PERTE_OPEX_MODE"],
                 param["PERTE_OPEX_MAX"]
             )
-
             FC = np.random.beta(
                 param["FC_ALPHA"],
                 param["FC_BETA"]
             )
-
             electricity_price = np.random.normal(
                 ELECTRICITY_PRICE_MU,
                 ELECTRICITY_PRICE_SIGMA
             )
-
-            opex = (
-                loss_opex
-                * (power * 365 * 24)
-                * FC
-                * electricity_price
-            )
+            opex = (loss_opex
+                * (power * 365 * 24)* FC* electricity_price)
 
             gain_redevance = (
-                param["TAUX_REDEVANCE_PREL"][scenario]
-                * param["V_PREL_OUVERT"] * 1e4
-                + 9.1
-                * param["TAUX_REDEVANCE_Q"][scenario]
-                * param["V_REST_OUVERT"]
-            )
+                param["TAUX_REDEVANCE_PREL"][scenario]* param["V_PREL_OUVERT"] * 1e4
+                + 9.1* param["TAUX_REDEVANCE_Q"][scenario] * param["V_REST_OUVERT"])
 
         else:
 
@@ -97,26 +82,24 @@ def montecarlo_parameters(n_sim, cnpe_choice, scenario):
                 param["K_OPEX_MODE"],
                 param["K_OPEX_MAX"]
             )
-
             opex = capex * k
-
-            gain_redevance = (
-                5
+            gain_redevance = (5
                 * param["TAUX_REDEVANCE_Q"][scenario]
-                * param["V_REST"]
-            )
+                * param["V_REST"])
 
-        # -------------------------
         # Perte de production moyenne
-        # -------------------------
         loss_prod_mean = (loss_2030 + loss_2050) / 2
+        CF_2030 = -opex + gain_redevance + loss_2030
+        CF_2050 = -opex + gain_redevance + loss_2050
 
         capex_results.append(capex)
         opex_results.append(opex)
         redevance_results.append(gain_redevance)
         loss_prod_results.append(loss_prod_mean)
-
+        NPV,ROI = compute_NPV(lifetime,capex,wacc,CF_2030,CF_2050)
     return {
+        "NPV" : np.array(NPV),
+        "ROI" : np.array(ROI),
         "CAPEX": np.array(capex_results),
         "OPEX": np.array(opex_results),
         "REDEVANCE": np.array(redevance_results),
@@ -156,180 +139,91 @@ if __name__ == "__main__":
         print(CNPE)
         results[CNPE] = {}
 
-
         for scenario in scenarios:
-    
-            data = montecarlo_parameters(
-                n_sim,
-                CNPE,
-                scenario
-            )
-
+            data = montecarlo_parameters(n_sim,CNPE,scenario)
             results[CNPE][scenario] = {
+                "NPV" : np.mean(data["NPV"])/1e6,
+                "ROI" : np.mean(data["ROI"])/1e6,
                 "CAPEX": np.mean(data["CAPEX"]) / 1e6,
                 "OPEX": np.mean(data["OPEX"]) / 1e6,
                 "REDEVANCE": np.mean(data["REDEVANCE"]) / 1e6,
                 "LOSS_PROD": np.mean(data["LOSS_PROD"]) * 100
             }
 
-            print(scenario  )
-            print(np.mean(data["REDEVANCE"]) / 1e6)
-
             #plot_param (CNPE,scenarios,results)
 
-            continue
             print("\n--------------------------------")
             print(f"CNPE      : {CNPE}")
             print(f"Scenario  : {scenario}")
             print("--------------------------------")
 
-            # ----------------------------------------------------
             # Dossier de sortie
-            # ----------------------------------------------------
-
-            scenario_dir = os.path.join(
-                output_dir,
-                CNPE,
-                scenario
-            )
-
+            scenario_dir = os.path.join(output_dir, CNPE,scenario)
             os.makedirs(scenario_dir, exist_ok=True)
 
-
-            # ----------------------------------------------------
             # Monte Carlo
-            # ----------------------------------------------------
 
-            NPV_results, ROI_results = montecarlo_simulation(
-                n_sim,
-                CNPE,
-                scenario
-            )
-            data = {
-                "NPV": NPV_results,
-                "ROI": ROI_results
-            }
-
-            with open(
-                os.path.join(scenario_dir, "monte_carlo_results.pkl"),
-                "wb"
-            ) as f:
-                pickle.dump(data, f)
-            continue
-
-
-            # ====================================================
-            # STATISTIQUES
-            # ====================================================
+            NPV_results, ROI_results = data["NPV"],data["ROI"]
 
             mean = np.mean(NPV_results)
             median = np.median(NPV_results)
-
             std = np.std(NPV_results, ddof=1)
-
             SE = std / np.sqrt(n_sim)
-
             q025 = np.percentile(NPV_results, 2.5)
             q975 = np.percentile(NPV_results, 97.5)
-
             probability_positive = np.mean(NPV_results > 0)
-
             mean_roi = np.mean(ROI_results)
             std_roi = np.std(ROI_results, ddof=1)
 
+            with open(os.path.join(scenario_dir, "monte_carlo_results.pkl"), "wb" ) as f:
+                pickle.dump(data, f)
 
-            # ----------------------------------------------------
             # Stockage des résultats
-            # ----------------------------------------------------
-
             all_results.append({
-
                 "CNPE": CNPE,
                 "Scenario": scenario,
                 "N_sim": n_sim,
-
                 "NPV_mean_M€": mean / 1e6,
                 "NPV_median_M€": median / 1e6,
                 "NPV_std_M€": std / 1e6,
-
                 "NPV_SE_M€": SE / 1e6,
-
                 "NPV_2.5_percentile_M€": q025 / 1e6,
                 "NPV_97.5_percentile_M€": q975 / 1e6,
-
                 "Probability_NPV_positive": probability_positive,
-
                 "ROI_mean": mean_roi,
-                "ROI_std": std_roi
+                "ROI_std": std_roi,
+                "CAPEX": np.mean(data["CAPEX"]) / 1e6,
+                "OPEX": np.mean(data["OPEX"]) / 1e6,
+                "REDEVANCE": np.mean(data["REDEVANCE"]) / 1e6,
+                "LOSS_PROD": np.mean(data["LOSS_PROD"]) * 100
+
             })
 
-
-            # ====================================================
             # AFFICHAGE
-            # ====================================================
 
-            print(f"Mean NPV : {mean/1e6:.1f} M€")
-            print(f"Median NPV : {median/1e6:.1f} M€")
-            print(f"NPV standard deviation : {std/1e6:.1f} M€")
-
-            print(
-                f"95% interval : "
-                f"[{q025/1e6:.1f} ; {q975/1e6:.1f}] M€"
-            )
-
-            print(
-                f"Probability NPV > 0 : "
-                f"{probability_positive*100:.1f}%"
-            )
-
-            print(f"Mean ROI : {mean_roi*100:.1f}%")
-            print(f"ROI standard deviation : {std_roi*100:.1f}%")
-
+            print_results(mean,median,std,q025,q975,probability_positive,mean_roi,std_roi)
             plot_NPV(NPV_results,CNPE,scenario,os,scenario_dir)
-
             plot_ROI(ROI_results,CNPE,scenario,os,scenario_dir)
-
             plot_convergence(NPV_results,n_sim,CNPE,scenario,os,scenario_dir)
 
-            #  TORNADO
-
             tornado_plot(NPV_results,CNPE,scenario)
-
-            plt.savefig(os.path.join(scenario_dir,"tornado.png"),
-                dpi=300,
-                bbox_inches="tight"
-            )
-
+            plt.savefig(os.path.join(scenario_dir,"tornado.png"),dpi=300,bbox_inches="tight")
             plt.close()
-
 
             calc_sobol(CNPE,scenario)
-
-            plt.savefig(
-                os.path.join(scenario_dir,"sobol.png"),
-                dpi=300,
-                bbox_inches="tight"
-            )
-
+            plt.savefig(os.path.join(scenario_dir,"sobol.png"),dpi=300,bbox_inches="tight")
             plt.close()
 
-
-    # ============================================================
     # CSV FINAL
-    # ============================================================
 
     results_df = pd.DataFrame(all_results)
-
     csv_path = os.path.join(output_dir,"results.csv")
-
     results_df.to_csv( csv_path, index=False, sep=";")
 
     print("\n================================")
     print("ALL SIMULATIONS FINISHED")
     print("================================")
-
     print(f"Results saved to : {csv_path}")
-
     print("\nSummary:")
     print(results_df)
 
---
+++ Suivi sites/plot.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def print_results(mean,median,std,q025,q975,probability_positive,mean_roi,std_roi):
+    print(f"Mean NPV : {mean/1e6:.1f} M€")
+    print(f"Median NPV : {median/1e6:.1f} M€")
+    print(f"NPV standard deviation : {std/1e6:.1f} M€")
+
+    print(
+        f"95% interval : "
+        f"[{q025/1e6:.1f} ; {q975/1e6:.1f}] M€"
+    )
+
+    print(
+        f"Probability NPV > 0 : "
+        f"{probability_positive*100:.1f}%"
+    )
+
+    print(f"Mean ROI : {mean_roi*100:.1f}%")
+    print(f"ROI standard deviation : {std_roi*100:.1f}%")
+
+def plot_convergence(NPV_results,n_sim,CNPE,scenario,os,scenario_dir):
+    cum_mean = (np.cumsum(NPV_results)/ np.arange(1, n_sim + 1) )
+
+    plt.figure(figsize=(8, 5))
+
+    plt.plot(cum_mean / 1e6)
+
+    plt.xlabel("Number of simulations")
+    plt.ylabel("Mean NPV (M€)")
+
+    plt.title(
+        f"Monte Carlo convergence\n"
+        f"{CNPE} - {scenario}"
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(
+        os.path.join( scenario_dir, "convergence.png"),
+        dpi=300,
+        bbox_inches="tight"
+    )
+
+    plt.close()
+
+def plot_ROI(ROI_results,CNPE,scenario,os,scenario_dir) :
+    plt.figure(figsize=(8, 5))
+
+    plt.hist( ROI_results * 100, bins=100)
+
+    plt.xlabel("ROI (%)")
+    plt.ylabel("Frequency")
+
+    plt.title(
+        f"Monte Carlo distribution of ROI\n"
+        f"{CNPE} - {scenario}"
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(
+        os.path.join(   scenario_dir, "ROI_hist.png"),
+        dpi=300,
+        bbox_inches="tight"
+    )
+
+    plt.close()
+
+def plot_NPV(NPV_results,CNPE,scenario,os,scenario_dir):
+    plt.figure(figsize=(8, 5))
+    
+    plt.hist(
+        NPV_results / 1e6,
+        bins=100
+    )
+
+    plt.xlabel("NPV (M€)")
+    plt.ylabel("Frequency")
+
+    plt.title(
+        f"Monte Carlo distribution of NPV\n"
+        f"{CNPE} - {scenario}"
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(
+        os.path.join(
+            scenario_dir,
+            "NPV_hist.png"
+        ),
+        dpi=300,
+        bbox_inches="tight"
+    )
+
+    plt.close()
